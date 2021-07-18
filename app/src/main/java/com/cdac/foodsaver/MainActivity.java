6dcc42c2456ecf7f72303a17bd7422703603738f
Himani <himaniupreti001@gmail.com>
2021-07-19 03:22:10 +0530

    dashboard and nav

--- app/src/main/java/com/cdac/foodsaver/MainActivity.java
+++ app/src/main/java/com/cdac/foodsaver/MainActivity.java
@@ -42,7 +42,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
         imageView = findViewById(R.id.imageView);
         drawerLayout = findViewById(R.id.drawerLayout);
---
+++ app/src/main/java/com/cdac/foodsaver/addFoodItem.java
@@ -0,0 +1,14 @@
+package com.cdac.foodsaver;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class addFoodItem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_food_item);
+    }
+}
