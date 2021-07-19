abe43b38c0f87e41bdc67892dbbb3a808447b09f
Prashant <prashantrustagi24@gmail.com>
2021-07-20 00:19:59 +0530

    all orders complete

--- app/src/main/java/com/cdac/foodsaver/Profile.java
+++ app/src/main/java/com/cdac/foodsaver/Profile.java
@@ -3,12 +3,25 @@ package com.cdac.foodsaver;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
 
 public class Profile extends AppCompatActivity {
+    ImageView backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_profile);
+        backButton = findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 }
