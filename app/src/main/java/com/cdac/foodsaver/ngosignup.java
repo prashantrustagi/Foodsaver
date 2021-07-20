7279e500eec2b70a53da13d69e1eeaa5dd505f8d
Prashant <prashantrustagi24@gmail.com>
2021-07-21 01:22:05 +0530

    all orders complete

---
+++ app/src/main/java/com/cdac/foodsaver/ngosignup.java
@@ -0,0 +1,14 @@
+package com.cdac.foodsaver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ngosignup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ngosignup);
+    }
+}
