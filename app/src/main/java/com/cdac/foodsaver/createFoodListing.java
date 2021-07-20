ba820bf77315cea4a8a085478036950e71c594c2
Prashant <prashantrustagi24@gmail.com>
2021-07-20 12:18:59 +0530

    all orders complete

--- app/src/main/java/com/cdac/foodsaver/createFoodListing.java
+++ app/src/main/java/com/cdac/foodsaver/createFoodListing.java
@@ -44,12 +44,10 @@ public class createFoodListing extends AppCompatActivity {
 
 
         List<PendingOrder> pendingOrderList = new ArrayList<>();
-        pendingOrderList.add(new PendingOrder("ABC NGO", "Burger, Fries, Noodles, Soup...", "8.40pm", "Pickup(Akash 723)", R.drawable.burgerlisting));
-        pendingOrderList.add(new PendingOrder("BCD NGO", "Burger, Fries, Noodles, Soup...", "8.40pm", "Pickup(Akash 723)", R.drawable.burgerlisting));
-        pendingOrderList.add(new PendingOrder("CDE NGO", "Burger, Fries, Noodles, Soup...", "8.40pm", "Pickup(Akash 723)", R.drawable.burgerlisting));
-        pendingOrderList.add(new PendingOrder("EFG NGO", "Burger, Fries, Noodles, Soup...", "8.40pm", "Pickup(Akash 723)", R.drawable.burgerlisting));
-        pendingOrderList.add(new PendingOrder("XYZ NGO", "Burger, Fries, Noodles, Soup...", "8.40pm", "Pickup(Akash 723)", R.drawable.burgerlisting));
-        pendingOrderList.add(new PendingOrder("ZXC NGO", "Burger, Fries, Noodles, Soup...", "8.40pm", "Pickup(Akash 723)", R.drawable.burgerlisting));
+        pendingOrderList.add(new PendingOrder("Burger", "Veg X 2", "Perish ", "8:40PM", R.drawable.burgerlisting));
+        pendingOrderList.add(new PendingOrder("Burger", "Veg X 2", "Perish ", "8:40PM", R.drawable.burgerlisting));
+        pendingOrderList.add(new PendingOrder("Burger", "Veg X 2", "Perish ", "8:40PM", R.drawable.burgerlisting));
+        pendingOrderList.add(new PendingOrder("Burger", "Veg X 2", "Perish ", "8:40PM", R.drawable.burgerlisting));
         setPendingRecycler(pendingOrderList);
 
         postListing = findViewById(R.id.postListing);
