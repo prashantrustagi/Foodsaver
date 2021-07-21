43c20f2121bea4678e61ddc6c7640690dd2172ca
Prashant <prashantrustagi24@gmail.com>
2021-07-21 12:53:35 +0530

    ngo dashboard

---
+++ app/src/main/java/com/cdac/foodsaver/availrestaurant_layout.java
@@ -0,0 +1,14 @@
+package com.cdac.foodsaver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class availrestaurant_layout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_availrestaurant_layout);
+    }
+}
---
+++ app/src/main/java/com/cdac/foodsaver/ngodashboard.java
@@ -0,0 +1,14 @@
+package com.cdac.foodsaver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ngodashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ngodashboard);
+    }
+}
